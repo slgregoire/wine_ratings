b93d73cae54cb11b0d8699d6e75f87561c5bcef9
volk0ff <slgregoire@gmail.com>
2015-05-06 12:13:49 -0400

    removed search limit

--- app/app/helpers/fuzzy_search.py
+++ app/app/helpers/fuzzy_search.py
@@ -8,7 +8,6 @@ def fuzzy_search(search):
 	#import json file with wine ratings/reviews into pandas dataframe
 	df_ratings = pd.read_json('../data/wine_ratings.json')
 	#execute fuzzy search and save results
-	df_ratings = df_ratings[:100]
 	df_ratings['score'] = df_ratings['name'].map(lambda x: fuzz.token_set_ratio(search, x))
 	df_sorted = df_ratings.sort(columns='score', ascending=False)
 
